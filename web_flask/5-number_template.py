98253118a6cae52df68ea079e7f63a5eef0fac3a
Susan <shi4git@gmail.com>
2023-05-21 22:01:09 -0700

    number templae

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ start a flask web application"""
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
